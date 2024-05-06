7908552723d3f3dec8e59b4f2119f0da0fc637ea
bryan <bryan.condori@tecsup.edu.pe>
2024-05-06 17:53:27 -0500

    Initial Commit

---
+++ ApellidosColeccionDeJuegos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ApellidosColeccionDeJuegos
+//
+//  Created by Bryan Condori Cahui on 6/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
